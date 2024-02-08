2ed6ba6ca0c5ed974d2d8c29936f224e59b6f247
mieras <mieras@gmail.com>
2024-02-08 18:56:28 +0100

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,10 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Jip Lucy Mieras'
+export const SITE_DESCRIPTION =	'Hi, Im Jip.'
+export const NAME_REAL = 'Jip'
+export const NAME = 'Jip Mieras'
+export const TERMS = ''
+export const KVK = ''
+export const IBAN = ''
